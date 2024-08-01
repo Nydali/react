32b98869695560b59df93d5ad7c488f96f935dc7
Nydali <Saints2812@gmail.com>
2024-08-01 14:13:54 +0200

    Komponent

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MyComponent from './components/MyComponent'
+
+function App() {
+  return (
+    <>
+      <MyComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ react/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaBeer } from 'react-icons/fa';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Hello from MyComponent</h1>
+      <p>Have a <FaBeer /> cheers!</p>
+    </div>
+  );
+};
+
+export default MyComponent;
